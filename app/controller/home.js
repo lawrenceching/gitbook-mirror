ac406b31898be92f77c90aed5dd014c6da874466
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-03-29 23:17:23 +0800

    Cache HTML to speed up the response

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,29 +2,42 @@
 
 const Controller = require('egg').Controller;
 const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
+const _ = require('lodash');
+
+const cache = {};
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
 
-    if(ctx.req.url === '/') {
+    if (ctx.req.url === '/') {
       ctx.status = 302;
       ctx.set('Location', '/v/zh-cn/');
       return;
     }
 
-    const target = 'https://imlc.me' + ctx.req.url;
-    ctx.logger.info('>>> ' + target);
-    const res = await fetch(target);
+    const getHtml = cache[ctx.req.url] || _.debounce(async () => {
 
-    if(!res.ok) throw new Error('Source website is not accessible');
+      const target = 'https://imlc.me' + ctx.req.url;
+      ctx.logger.info('>>> ' + target);
+      try {
+        const res = await fetch(target);
+        if (!res.ok) return '<html><body><p>500 Server Internal Error</p></body></html>';
+        const html = await res.text();
+        return html;
+      } catch (e) {
+        this.ctx.logger.warn(e.message);
+        return '<html><body><p>500 Server Internal Error</p></body></html>';
+      }
 
-    const html = await res.text();
-    // await fs.promises.writeFile(path.join('run', 'cache', 'index.html'), html, 'utf8');
+    }, this.ctx.app.config.cacheExpireTime, {
+      leading: true,
+      trailing: false,
+    });
 
-    ctx.body = html;
+    cache[ctx.req.url] = getHtml;
+
+    ctx.body = await getHtml();
   }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,9 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = exports = {
+    cacheExpireTime: 60 * 60 * 1000,
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1584071456274_9195';
